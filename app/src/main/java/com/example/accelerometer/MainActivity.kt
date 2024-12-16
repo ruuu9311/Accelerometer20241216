3c827d6f203f55ed36051c3931e0cc81ef4420b9
ruuu9311 <ruuu9311@gmail.com>
2024-12-16 16:57:51 +0800

    v0_

--- app/src/main/java/com/example/accelerometer/MainActivity.kt
+++ app/src/main/java/com/example/accelerometer/MainActivity.kt
@@ -31,14 +31,8 @@ import android.hardware.SensorManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.IntOffset
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
